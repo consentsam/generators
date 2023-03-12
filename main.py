9402ebcb84525257ca6fceb1a5279508a1d91f85
SATYAJEET SINDHIYANI <samxan2014@gmail.com>
2023-03-12 06:06:47 +0530

    feat: Implement find_generator function

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from csv_writer import CSVWriter
 
 def find_generator(p):
     # Write your code here
-    pass
+    smallest_prime_number = 2
+    for i in range(2,p):
+        smallest_prime_number = i;
+        set_of_all_generated_numbers = set()
+        for j in range(0,p):
+            generated_number = pow(i,j,p)
+            set_of_all_generated_numbers.add(generated_number)
+            if len(set_of_all_generated_numbers)==p-1: # because we have not added 0 here 
+                return smallest_prime_number
 
 
 csv_writer = CSVWriter()
